4fa794db6f1d5aee5b33cd01bd2db94af45afa89
lokman <kalinin5000@yandex.ru>
2022-12-07 17:05:22 +0300

    Решение третей задачи

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,17 +1,21 @@
 ﻿try
 {
-int[] mass = new int[8];
-for (int i = 0; i < mass.Length; i++)
-{
-    Console.WriteLine($"Введите элемент массива под номером {i}.");
-    mass[i] = Convert.ToInt32(Console.ReadLine());
-}
-Console.WriteLine();
-Console.Write("Введеный массив: ");
-for (int j = 0; j < mass.Length; j++)
-{
-    Console.Write($"{mass[j]} ");
-}
+    void MassRide()
+    {
+        int[] mass = new int[8];
+        for (int i = 0; i < mass.Length; i++)
+        {
+            Console.WriteLine($"Введите элемент массива под номером {i}.");
+            mass[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine();
+        Console.Write("Введеный массив: ");
+        for (int j = 0; j < mass.Length; j++)
+        {
+            Console.Write($"{mass[j]} ");
+        }
+    }
+    MassRide();
 }
 catch
 {
