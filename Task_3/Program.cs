db51a2e7c0f352a1b2b7197721fbcb75a93e00e0
lokman <kalinin5000@yandex.ru>
2022-12-07 16:57:38 +0300

    Решение третей задачи

---
+++ Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] mass = new int[8];
+for (int i = 0; i < mass.Length; i++)
+{
+    Console.WriteLine($"Введите элемент массива под номером {i}.");
+    mass[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+Console.Write("Введеный массив: ");
+for (int j = 0; j < mass.Length; j++)
+{
+    Console.Write($"{mass[j]} ");
+}
