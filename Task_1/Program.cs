cb2a6d8eef3cd52acf5893baaff763ba7e1958ff
lokman <kalinin5000@yandex.ru>
2022-12-07 16:27:59 +0300

    Решение первой задачи

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число.");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степерь");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int Step(int num, int st)
+{
+    int save = num;
+    for (int i = 1; i < st; i++)
+    {
+        save = save * num;
+    }
+    return save;
+}
+Console.WriteLine();
+int res = Step(num1, num2);
+Console.WriteLine($"Число {num1} в степени {num2} равно {res}.");
