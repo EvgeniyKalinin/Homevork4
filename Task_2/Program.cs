9be0d0bd6a718e1c8c0da70abc5d3573f79a4fd5
lokman <kalinin5000@yandex.ru>
2022-12-07 16:32:50 +0300

    Решение второй задачи

---
+++ Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число.");
+int num = Convert.ToInt32(Console.ReadLine());
+int CountNumber(int num)
+{
+    int save = num;
+    int count = 0;
+    while (num > 0)
+    {
+        int ost = num % 10;
+        count = count + ost;
+        num = num / 10;
+    }
+    return count;
+}
+
+int res = CountNumber(num);
+Console.WriteLine($"Сумма цифр в числе {num} равна {res}.");
